7e4e9731df24254619fc1c763713059161711e47
luongdo03x-byte <luongdo03x@gmail.com>
2026-08-17 13:19:00 +0700

    test: support factory v2 local unit tests

--- android/account-factory/app/build.gradle.kts
+++ android/account-factory/app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20240303")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
