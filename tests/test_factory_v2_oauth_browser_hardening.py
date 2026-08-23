2523eed7c0387c77211a4d9b6c139b4f3ab31421
luongdo03x-byte <luongdo03x@gmail.com>
2026-08-23 20:04:42 +0700

    test: cover hardened OAuth browser auto-login setup

---
+++ tests/test_factory_v2_oauth_browser_hardening.py
@@ -0,0 +1,154 @@
+import unittest
+
+from core.factory_v2.ui_automation.browser.flow import BrowserLoginFlow
+from core.factory_v2.ui_automation.browser.screens import build_browser_detector
+from core.factory_v2.ui_automation.detector import DetectedScreen
+from core.factory_v2.ui_automation.driver import ActionResult
+from core.factory_v2.ui_automation.hierarchy import UiHierarchyReader
+from core.factory_v2.ui_automation.flow_result import FlowResult
+from core.factory_v2.worker_protocol import WorkerCommand
+from workers.account_factory_worker import WorkerAgent
+
+
+class FakeSocialFlow:
+    def __init__(self):
+        self.driver = type("Driver", (), {
+            "open_package": lambda self, package: None,
+            "detect_screen": lambda self: DetectedScreen("UNKNOWN", 0.0, ()),
+        })()
+
+    def run(self, profile, *, account_id=None):
+        return FlowResult("running", "UNKNOWN")
+
+    def observe_checkpoint(self, *args, **kwargs):
+        return FlowResult("running", "UNKNOWN")
+
+
+class FakeBrowserFlow:
+    def __init__(self):
+        self.prepare_calls = 0
+
+    def prepare_browser(self):
+        self.prepare_calls += 1
+        return FlowResult("running", "BROWSER_READY")
+
+    def run(self, username, password):
+        return FlowResult("running", "LOGIN_SUCCEEDED")
+
+
+class FakeAvd:
+    adb = "adb"
+    runner = type("Runner", (), {
+        "run": lambda *args, **kwargs: type(
+            "Result", (), {"returncode": 0, "stdout": "", "stderr": ""}
+        )()
+    })()
+
+    def __init__(self):
+        self.events = []
+
+    def reset_browser_session(self, serial, browser_package):
+        self.events.append(("reset", serial, browser_package))
+
+    def open_package(self, serial, package):
+        self.events.append(("open_package", serial, package))
+
+    def set_package_enabled(self, serial, package, enabled):
+        self.events.append(("package_enabled", serial, package, bool(enabled)))
+
+    def open_url(self, serial, url, *, browser_package=None):
+        self.events.append(("open_url", serial, url, browser_package))
+
+
+class FirstRunDriver:
+    def __init__(self):
+        self.actions = []
+
+    def wait_for(self, screens, timeout):
+        self.actions.append(("wait_for", tuple(screens)))
+        return DetectedScreen("CHROME_FIRST_RUN", 0.99, ("use_without_account",))
+
+    def tap_use_without_account(self):
+        self.actions.append(("tap", "use_without_account"))
+        return ActionResult("completed", before="CHROME_FIRST_RUN", after="UNKNOWN")
+
+
+class OAuthBrowserHardeningTests(unittest.TestCase):
+    def make_agent(self):
+        avd = FakeAvd()
+        browser_flow = FakeBrowserFlow()
+        agent = WorkerAgent(
+            "worker-1",
+            "acp-worker-01",
+            "emulator-5554",
+            avd=avd,
+            instagram_flow=FakeSocialFlow(),
+            threads_flow=FakeSocialFlow(),
+            browser_login_flow=browser_flow,
+        )
+        return agent, avd, browser_flow
+
+    def test_chrome_first_run_is_detected_only_with_exact_skip_control(self):
+        xml = """<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
+        <hierarchy rotation='0'>
+          <node index='0' text='Welcome to Chrome' resource-id='' class='android.widget.TextView'
+                package='com.android.chrome' content-desc='' clickable='false' enabled='true'
+                bounds='[0,0][400,100]' />
+          <node index='1' text='Use without an account' resource-id='' class='android.widget.Button'
+                package='com.android.chrome' content-desc='' clickable='true' enabled='true'
+                bounds='[20,700][380,780]' />
+        </hierarchy>"""
+        snapshot = UiHierarchyReader().parse(
+            xml,
+            package="com.android.chrome",
+            activity="ChromeActivity",
+        )
+
+        detected = build_browser_detector().detect(snapshot)
+
+        self.assertEqual("CHROME_FIRST_RUN", detected.kind)
+        self.assertFalse(detected.protected)
+        self.assertTrue(detected.automation_allowed)
+
+    def test_prepare_browser_skips_chrome_first_run_before_oauth_navigation(self):
+        driver = FirstRunDriver()
+        flow = BrowserLoginFlow(driver)
+
+        result = flow.prepare_browser()
+
+        self.assertEqual("running", result.status)
+        self.assertEqual("BROWSER_READY", result.screen)
+        self.assertEqual(("tap", "use_without_account"), driver.actions[-1])
+
+    def test_open_url_prepares_chrome_and_disables_threads_before_navigation(self):
+        agent, avd, browser_flow = self.make_agent()
+        url = "https://threads.net/oauth/authorize?state=one"
+
+        agent.execute(WorkerCommand("open-1", "OPEN_URL", "acc-1", {"url": url}))
+
+        self.assertEqual(1, browser_flow.prepare_calls)
+        self.assertEqual(
+            [
+                ("reset", "emulator-5554", "com.android.chrome"),
+                ("open_package", "emulator-5554", "com.android.chrome"),
+                ("package_enabled", "emulator-5554", "com.instagram.barcelona", False),
+                ("open_url", "emulator-5554", url, "com.android.chrome"),
+            ],
+            avd.events,
+        )
+
+    def test_restore_oauth_apps_reenables_threads_after_browser_session(self):
+        agent, avd, _ = self.make_agent()
+        url = "https://threads.net/oauth/authorize?state=one"
+        agent.execute(WorkerCommand("open-1", "OPEN_URL", "acc-1", {"url": url}))
+
+        agent.execute(WorkerCommand("restore-1", "RESTORE_OAUTH_APPS", "acc-1", {}))
+
+        self.assertEqual(
+            ("package_enabled", "emulator-5554", "com.instagram.barcelona", True),
+            avd.events[-1],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
